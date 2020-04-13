d2ab39a413f7d677c2966986eed43525c55bcee2
mh-bennett <mh.bennett.dev@gmail.com>
2020-04-13 10:06:55 -0700

    Add func to automate provisioning an S3 website.

--- 01-webotron/webotron/webotron.py
+++ 01-webotron/webotron/webotron.py
@@ -1,7 +1,7 @@
 import boto3
 import sys
 import click
-
+from botocore.exceptions import ClientError
 # pylint: disable=E1101
 # ^ Done due to an incompatibility between pylint and Boto3 Dynamic typing
 session = boto3.Session(profile_name='mb-aws-acct-cli')
@@ -25,6 +25,52 @@ def list_bucket_objects(bucket):
     "List objects in an S3 bucket"
     for obj in s3.Bucket(bucket).objects.all():
         print(obj)
+
+@cli.command('setup-bucket')
+@click.argument('bucket')
+def setup_bucket(bucket):
+    "Create and Configure an S3 bucket for static website hosting"
+    s3_bucket = None
+    try: 
+        s3_bucket = s3.create_bucket(
+            Bucket=bucket, 
+            CreateBucketConfiguration={'LocationConstraint' : session.region_name}
+        )
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'BucketAlreadyOwnedByYou':
+            s3_bucket =  s3.Bucket(bucket)
+        else:
+            raise e
+
+    policy = """
+    {
+        "Version":"2012-10-17",
+        "Statement":[{
+            "Sid":"PublicReadGetObject",
+            "Effect":"Allow",
+            "Principal": "*",
+            "Action":["s3:GetObject"],
+            "Resource":["arn:aws:s3:::%s/*"]
+        }]
+    }
+    """ % s3_bucket.name
+    policy =  policy.strip()
+    pol = s3_bucket.Policy()
+    pol.put(Policy=policy)
+
+    web = s3_bucket.Website()
+    web.put(WebsiteConfiguration={
+        'ErrorDocument': {
+            'Key': 'error.html'
+        },
+        'IndexDocument': {
+            'Suffix': 'index.html'
+        }
+    })
+
+    return
+
+
 
 if __name__ == "__main__":
     cli()
